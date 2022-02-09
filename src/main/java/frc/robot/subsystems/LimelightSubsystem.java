76ad7bb852fc230a7bd9d737a638377ad2e3355c
Chris 'Xenon' Hanson <xenon@alphapixel.com>
2022-02-08 19:23:04 -0700

    Refactors LimelightSubsystem

--- src/main/java/frc/robot/subsystems/LimelightSubsystem.java
+++ src/main/java/frc/robot/subsystems/LimelightSubsystem.java
@@ -8,25 +8,32 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class LimelightSubsystem extends SubsystemBase {
   private NetworkTableEntry m_tve, m_txe, m_tye, m_tae;
+  private double m_tv, m_tx, m_ty, m_ta;
+
+  public double getValid() { return(m_tv);}
+  public double getX() { return(m_tx);}
+  public double getY() { return(m_ty);}
+  public double getArea() { return(m_ta);}
 
     @Override
-    public void periodic()
-    {
+    public void periodic() {
         //read values periodically
-        double tv   = m_tve.getDouble(0.0);
-        double tx   = m_txe.getDouble(0.0);
-        double ty   = m_tye.getDouble(0.0);
-        double area = m_tae.getDouble(0.0);
+        m_tv = m_tve.getDouble(0.0);
+        m_tx = m_txe.getDouble(0.0);
+        m_ty = m_tye.getDouble(0.0);
+        m_ta = m_tae.getDouble(0.0);
     
         //post to smart dashboard periodically
-        SmartDashboard.putNumber("LimelightValid", tv);
-        SmartDashboard.putNumber("LimelightX", tx);
-        SmartDashboard.putNumber("LimelightY", ty);
-        SmartDashboard.putNumber("LimelightArea", area);
+        SmartDashboard.putNumber("LimelightValid", m_tv);
+        SmartDashboard.putNumber("LimelightX", m_tx);
+        SmartDashboard.putNumber("LimelightY", m_ty);
+        SmartDashboard.putNumber("LimelightArea", m_ta);
     }
 
     public LimelightSubsystem() {
         NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+
+        m_tv = m_tx = m_ty = m_ta = 0.0;
     
         m_tve = table.getEntry("tv");
         m_txe = table.getEntry("tx");

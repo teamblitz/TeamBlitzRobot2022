ee94d9c165c38b36bf3e8a7925e4070611217c9f
Noah <40500696+NoahStricker@users.noreply.github.com>
2022-03-29 06:47:13 -0600

    worked more on vision

--- src/main/java/frc/robot/subsystems/VisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -9,12 +9,16 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 // Condenses all Limelight and Plan Subsystems into one Subsystem to decrease verbosity 
 public class VisionSubsystem extends SubsystemBase{
 
-    private LimelightCamera ballLimeLimelight;
-    private LimelightCamera targetLimeLight;
-
+    private LimelightCamera ballLimelight;
+    private LimelightCamera targetLimelight;
+    public final BallAcquirePlan ballAcquirePlan; // The object that this variable referances can't change, this doesn't stop the object from changing.
+    public final BallShooterPlan ballShooterPlan; // Same here.
 
     public VisionSubsystem() {
-        ballLimeLimelight = new LimelightCamera("limelight", 0);
+        ballLimelight = new LimelightCamera("limelight", 0);
+        targetLimelight = new LimelightCamera("limelight-target", 7);
+        ballAcquirePlan = new BallAcquirePlan(ballLimelight);
+        ballShooterPlan = new BallShooterPlan(targetLimelight, null);
     }
     // Instantiated for each limelight
     private class LimelightCamera {
@@ -74,7 +78,7 @@ public class VisionSubsystem extends SubsystemBase{
 
     }
     
-    public class BallAcquirePlan {
+    private class BallAcquirePlan {
         // all of the below can be tinkered with for tuning
         private double m_autoRotationScaleFactor = 0.3;
         private double m_autoMoveScaleFactor = 0.9;
@@ -149,4 +153,88 @@ public class VisionSubsystem extends SubsystemBase{
 
         
     }
+
+    private class BallShooterPlan {
+        // all of the below can be tinkered with for tuning
+        private double m_autoRotationScaleFactor = 0.3;
+        private double m_autoMoveScaleFactor = 0.9;
+        private double m_Kp = -0.1f;
+        private double m_min_command = 0.05f;
+        private double m_maxHeadingError = 10.0;
+        private double m_maxOffsetFraction = 2.0;
+        private double m_maxDriveSpeedFraction = 0.45; // how fast we allow the autodrive code to dictate we want to go
+
+        private LimelightCamera m_limelight;
+        // private StatusLightSubsystem m_statusLightSubsystem;
+
+        // these are the calculated movement directives for autodrive
+        private double m_fwd = 0;
+        private double m_rot = 0;
+
+        public BallShooterPlan(LimelightCamera limelight, StatusLightSubsystem statusLightSubsystem) {
+            m_limelight = limelight;
+            // m_statusLightSubsystem = statusLightSubsystem;
+        }
+
+        public double getFwd() {return(m_fwd);}
+        public double getRot() {return(m_rot);}
+        
+
+        public void periodic() { // We must call this manualy
+            // TODO <<<>>> set correct pipeline number
+            // No need to update this periodicly
+            // m_limelight.setPipeline(7);
+
+            m_rot = 0.0;
+            m_fwd = 0.0;
+            // acquire tx and area and valid from LimelightSubsystem
+            double tx = m_limelight.getX();
+            double ty = m_limelight.getY();
+
+            // boolean valid = false;
+
+            if(m_limelight.getValid() > 0)
+            {
+                double heading_error = -tx; // there's a negative here that's negated again later in arcadeDrive()
+
+                // clamp tx magnitude for safety
+                heading_error = Math.max(-m_maxHeadingError, heading_error);
+                heading_error = Math.min(heading_error, m_maxHeadingError);
+
+                double steering_adjust = 0.0f;
+                if (tx > 1.0)
+                {
+                        steering_adjust = m_Kp*heading_error - m_min_command;
+                }
+                else if (tx < 1.0)
+                {
+                        steering_adjust = m_Kp*heading_error + m_min_command;
+                }
+
+                // calculate the direction we need to drive to aim the target where we want it
+                // if target is below crosshair, then ty will be negative, and otherwise positive.
+                // if ty is negative then we are too far back and need to move forward, so direction will be positive 1
+                // if ty is positive then we are too close and need to move back so direction will be negative 1
+                double driveDirection = (ty < -1 ? 1 : -1);
+                // calculate the speed to drive at based on how far off from target we are
+                double driveSpeedFraction = 0.33 * (Math.abs(ty)) / m_maxOffsetFraction; // results in [0.0 ... 1.0]
+                // limit the drive speed fraction to m_maxDriveSpeedFraction for safety
+                driveSpeedFraction = Math.min(m_maxDriveSpeedFraction, driveSpeedFraction);
+                
+                // DON'T negate steering adjust because target camera faces the front
+                m_rot = m_autoRotationScaleFactor * steering_adjust * -1;
+                // -1 below flips the drive direction compared to BallAcquire which faces the rear instead of the front
+                m_fwd = m_autoMoveScaleFactor * driveSpeedFraction * driveDirection * -1;
+
+                // we could post the debug info to the Shuffleboard if we wanted
+                SmartDashboard.putNumber("AutoTarget", (m_autoRotationScaleFactor * driveSpeedFraction * -1));
+                SmartDashboard.putNumber("GetFwd", (m_fwd));
+
+            }
+
+        }
+
+        
+    }
+
 }

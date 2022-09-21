5f2442782b4e966cca2b6251ccbacd84f1a306da
teamblitz2083 <73154888+teamblitz2083@users.noreply.github.com>
2022-09-20 20:02:29 -0600

    stuff, also prob broken

---
+++ src/main/java/frc/robot/subsystems/AutonomousSubsystem.java
@@ -0,0 +1,30 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class AutonomousSubsystem extends SubsystemBase{
+    
+    // Have we left the tarmat?
+    private boolean hasLeftTarmat;
+    // How much time is left?
+    private long autoTimeLeftMS;
+
+    // Should we use vision or dead reakoning?
+    private boolean useVision;
+
+
+    @Override
+    public void periodic() {
+        if (DriverStation.isAutonomous()) {
+            autoPeriodic();
+        }
+    }
+
+
+    private void autoPeriodic() {
+        autoTimeLeftMS =  Math.round(1000* DriverStation.getMatchTime());
+
+        
+    }
+}

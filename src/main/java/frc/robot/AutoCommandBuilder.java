bd44b9e9d0ae24e739a3270b0792b9feef003727
teamblitz2083 <73154888+teamblitz2083@users.noreply.github.com>
2022-09-20 20:01:07 -0600

    Broke stuff

---
+++ src/main/java/frc/robot/AutoCommandBuilder.java
@@ -0,0 +1,12 @@
+package frc.robot;
+
+public class AutoCommandBuilder {
+
+    public AutoCommandBuilder() {
+        // Get subs here
+    }
+
+    public Command buildCommand() {
+        
+    }
+}
